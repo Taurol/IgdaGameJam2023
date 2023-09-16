f4e28dd90a385ef4dce817febfba2dfd7323d078
Marcos <marcos@creadores.com.py>
2023-09-16 18:06:29 -0400

    feat: add anim logic

--- Assets/Player/Player.gd
+++ Assets/Player/Player.gd
@@ -10,6 +10,13 @@ extends CharacterBody2D
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
+var current_anim: String = "idle"
+
+signal falling
+signal jumping
+signal idle
+signal walking
+
 func _start() -> void:
 	velocity = Vector2.ZERO
 
@@ -22,6 +29,8 @@ func process_movement(delta: float) -> void:
 	print(velocity)
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump()
+	if !$AnimationPlayer.current_animation == current_anim:
+		$AnimationPlayer.play(current_anim)
 	move_and_slide()
 
 func get_gravity() -> float:
@@ -31,13 +40,20 @@ func get_gravity() -> float:
 
 func jump():
 	velocity.y = jump_velocity
+	emit_signal("jumping")
 
 func get_input_velocity() -> float:
 	var horizontal := 0.0
 	
 	if Input.is_action_pressed("left"):
 		horizontal -= 1.0
+		$Sprite2D.scale.y = -1
 	if Input.is_action_pressed("right"):
 		horizontal += 1.0
+		$Sprite2D.scale.y = 1
 	
+	if horizontal == 0.0:
+		emit_signal("idle")
+	else:
+		emit_signal("walking")
 	return horizontal
